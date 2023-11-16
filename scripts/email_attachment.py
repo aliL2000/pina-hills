0707b89b606679f052e9d85e1c2c6809931a259a
aliL2000 <aliladha2000@gmail.com>
2023-11-16 12:22:43 -0500

    moidfinyg email attachment script

--- scripts/email_attachment.py
+++ scripts/email_attachment.py
@@ -1,20 +1,22 @@
 import email
 import os
 
-path = 'data/email/'
-listing = os.listdir(path)
-
-for fle in listing:
-    print(fle)
-
-    if str.lower(fle[-3:])=="eml":
-        msg = email.message_from_file(open("data/email/"+fle))
-        attachments=msg.get_payload()
-        for attachment in attachments:
-            try:
-                fnam=attachment.get_filename()
-                f=open(fnam, 'wb').write(attachment.get_payload(decode=True,))
-                f.close()
-            except Exception as detail:
-                #print detail
-                pass
+def download_attachments(path):
+    listing = os.listdir(path)
+    countSuccess = 0
+    countFail = 0
+    for fle in listing:
+        if str.lower(fle[-3:])=="eml":
+            msg = email.message_from_file(open("data/email/"+fle))
+            attachments=msg.get_payload()
+            
+            for attachment in attachments:
+                try:
+                    fnam=attachment.get_filename()
+                    fnam = fnam.replace("\n","")
+                    f=open("data/emailattachment/"+fnam, 'wb').write(attachment.get_payload(decode=True,))
+                    countSuccess+=1
+                except Exception as detail:
+                    countFail+=1
+                    pass
+    print(f"Downloaded {countSuccess}/{countSuccess+countFail}")
